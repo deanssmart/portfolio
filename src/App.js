a0bf6182a4d135290e8eb8c24f01444cd23fbf63
deanssmart <deanssmart@gmail.com>
2020-10-09 13:53:30 +0100

    image sizes reduced

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-// import Nav from './components/Nav/Nav';
+import Nav from './components/Nav/Nav';
 import Skills from './components/Skills';
 import logo from '../src/img/logo.png';
 
--- src/js/DraggableImage/index.js
+++ src/js/DraggableImage/index.js
@@ -9,14 +9,14 @@
 * http://www.codrops.com
 */
 import DraggableImage from "./DraggableImage";
-const imagesLoaded = require('imagesloaded');
+// const imagesLoaded = require('imagesloaded');
 
 // Preload images
-const preloadImages = () => {
-    return new Promise((resolve, reject) => {
-        imagesLoaded(document.querySelectorAll('.img-drag, .img-trail'), {background: true}, resolve);
-    });
-};
+// const preloadImages = () => {
+//     return new Promise((resolve, reject) => {
+//         imagesLoaded(document.querySelectorAll('.img-drag, .img-trail'), {background: true}, resolve);
+//     });
+// };
 
 // Preload fonts
 // const preloadFonts = () => {
@@ -30,10 +30,7 @@ const preloadImages = () => {
 //     });
 // };
 
-Promise.all([
-    preloadImages(),
-    // preloadFonts()  
-]).then(() => {
+Promise.all([]).then(() => {
     [...document.querySelectorAll('.img-wrap')].forEach((element) => new DraggableImage(element));
     document.body.classList.remove('loading');
 });
