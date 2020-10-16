57a572272a71a3c535432eff6b8d3735d28cfa19
deanssmart <deanssmart@gmail.com>
2020-10-16 12:10:54 +0100

    Updates

---
+++ precache-manifest.7d6aabaff087bc004d391081fbd81ba6.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb002f01aa032c9668c3858a551e1286",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "80833b750d9d7f7b5d44",
+    "url": "/portfolio/static/css/main.5206cd0c.chunk.css"
+  },
+  {
+    "revision": "df61052c288a92120487",
+    "url": "/portfolio/static/js/2.24758c12.chunk.js"
+  },
+  {
+    "revision": "f0355c8987846e116e8c564d6462640c",
+    "url": "/portfolio/static/js/2.24758c12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80833b750d9d7f7b5d44",
+    "url": "/portfolio/static/js/main.93b81166.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "8d7525481c809205be36a63828d5893d",
+    "url": "/portfolio/static/media/QuiveraRegular.8d752548.otf"
+  },
+  {
+    "revision": "910649f298fd378dbb75a0a234af7f33",
+    "url": "/portfolio/static/media/aeroblade.910649f2.webp"
+  },
+  {
+    "revision": "2f69a86b62cb67f3e77b0840dad382bf",
+    "url": "/portfolio/static/media/artist.2f69a86b.svg"
+  },
+  {
+    "revision": "26d4ca2be1a6c105c9b14547f94b929b",
+    "url": "/portfolio/static/media/coder.26d4ca2b.svg"
+  },
+  {
+    "revision": "fddcafb2b150446c03586c7e5d3567f6",
+    "url": "/portfolio/static/media/css.fddcafb2.webp"
+  },
+  {
+    "revision": "1e93145a3b7f2bb7a4c48a10113ab987",
+    "url": "/portfolio/static/media/damper.1e93145a.webp"
+  },
+  {
+    "revision": "272e25f62bade174a78695195010cca9",
+    "url": "/portfolio/static/media/db11.272e25f6.webp"
+  },
+  {
+    "revision": "37fc8e03963b78860d52b2a008240758",
+    "url": "/portfolio/static/media/dyson.37fc8e03.webp"
+  },
+  {
+    "revision": "d1b53c694db486066ef30fe658eb4ec7",
+    "url": "/portfolio/static/media/email.d1b53c69.svg"
+  },
+  {
+    "revision": "65a0d8b31e2b9c907eafe3ea9c6169a1",
+    "url": "/portfolio/static/media/engineer.65a0d8b3.svg"
+  },
+  {
+    "revision": "d015aad3b0ec556a185d522ec4c7fc4a",
+    "url": "/portfolio/static/media/frankenstyle.d015aad3.webp"
+  },
+  {
+    "revision": "13c2d303bae73524959ad00f16388248",
+    "url": "/portfolio/static/media/fs.13c2d303.webp"
+  },
+  {
+    "revision": "1686c1a6350d86e97061a980244e63bb",
+    "url": "/portfolio/static/media/github.1686c1a6.svg"
+  },
+  {
+    "revision": "8154175a6e8b02bdfd4cb259f907d063",
+    "url": "/portfolio/static/media/her.8154175a.webp"
+  },
+  {
+    "revision": "58c634e7333a5076e3b81091edd97052",
+    "url": "/portfolio/static/media/html.58c634e7.webp"
+  },
+  {
+    "revision": "59ab8eaf3e08a5ac554b3688916be4a9",
+    "url": "/portfolio/static/media/js.59ab8eaf.webp"
+  },
+  {
+    "revision": "f0f0d580f32e224e444c3322f99fc33e",
+    "url": "/portfolio/static/media/laravel.f0f0d580.webp"
+  },
+  {
+    "revision": "8904f009bbe81e5e3bfd2bafad5590d2",
+    "url": "/portfolio/static/media/liam.8904f009.webp"
+  },
+  {
+    "revision": "f12989abd31444f092ecaee06585f4a4",
+    "url": "/portfolio/static/media/linkedin.f12989ab.svg"
+  },
+  {
+    "revision": "cccdd9f6d9e551044943cfe7fc1141cb",
+    "url": "/portfolio/static/media/logo.cccdd9f6.svg"
+  },
+  {
+    "revision": "7f8f796289b47f9a54404b5a2f99e8ee",
+    "url": "/portfolio/static/media/oldMan.7f8f7962.webp"
+  },
+  {
+    "revision": "9ba351a305277e38417bd928ded064ea",
+    "url": "/portfolio/static/media/patent.9ba351a3.webp"
+  },
+  {
+    "revision": "37f51e5e2bae1367c5b870c12c627d37",
+    "url": "/portfolio/static/media/php.37f51e5e.webp"
+  },
+  {
+    "revision": "3120623edafb2d4616cb04a003331a9c",
+    "url": "/portfolio/static/media/react.3120623e.webp"
+  },
+  {
+    "revision": "53fa9729e52df4dd673a34b893d5d074",
+    "url": "/portfolio/static/media/ronnie.53fa9729.webp"
+  },
+  {
+    "revision": "42b364dad8779ddf04cfb4f32f0693eb",
+    "url": "/portfolio/static/media/siam.42b364da.webp"
+  },
+  {
+    "revision": "46cf1922ce4ff961e8ba622219c5768d",
+    "url": "/portfolio/static/media/tourn.46cf1922.webp"
+  },
+  {
+    "revision": "4a1d4adf077d7bbd811427dd6763a0c0",
+    "url": "/portfolio/static/media/wilson.4a1d4adf.webp"
+  }
+]);
